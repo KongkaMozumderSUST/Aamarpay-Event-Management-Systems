e1c05168e3bd0fdfc69b45eb0366ae7d280eff87
KongkaMozumderSUST <kongkamozumder7@gmail.com>
2025-09-04 21:19:27 +0600

    my events

--- event-management-system/components/Navbar.tsx
+++ event-management-system/components/Navbar.tsx
@@ -1,35 +1,37 @@
 "use client"
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import { redirect,useRouter} from "next/navigation";
 import { useEffect, useState } from "react";
 export default function Navbar()
 {
     const [currentUserName,setCurrentUserName] = useState("")
+    const router = useRouter();
     useEffect(() => {
       const storedUser = localStorage.getItem("user");
       const currentUserName = storedUser ? JSON.parse(storedUser).name : "";
       setCurrentUserName(currentUserName);
+      router.refresh()
     },[])
  
   
   async function handleSubmitLogout(e: React.FormEvent) {
     e.preventDefault();
     localStorage.setItem("user","");
+    router.refresh();
     redirect('/')
   }
 return(
 <div className="bg-sky-950 flex items-center gap-100 h-20 p-4 shadow-xl">
   <div><a href="/" className="text-white">Home</a></div>
-  <div className="justify-end p-4 flex items-center gap-4">
+  <div className="justify-end p-4 flex items-center gap-40">
 {currentUserName?
   (<><div><a href="/MyEvents" className="text-white"><button>My Event</button></a></div>
   <div><Link href="/CreateEvents" className="text-white"><button>Create Event</button></Link></div>
-  <div>
+
     <div><a href="/" className="text-white"><button>{currentUserName}</button></a></div>
     <form onSubmit={handleSubmitLogout}>
     <button type="submit" className="text-white" >Logout</button>
     </form>
-    </div>
     
   </>
   ) :<>
